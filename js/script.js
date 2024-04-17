bbcca58de17b930b5b9487b96589e37901af0728
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-17 15:09:53 +0200

    feat: image and a little function

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Ciao come va?'
+            message: 'Ciao come va?',
+            imgName: "./img/flw.jpg",
+            altText: "lotr image",
+            btn: ""
+        }
+    },
+    methods: {
+        off: function () {
+            if(this.btn === "") {
+                this.imgName = "";
+                this.altText = "";
+                this.btn = "off";
+            } else {
+                this.imgName = "./img/flw.jpg";
+                this.altText = "lotr image";
+                this.btn = "";
+            }
         }
     }
 }).mount('#app')
